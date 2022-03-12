dbb227b3ecca248b566640ecacb73f7908c6d871
"David Oluwatosin" <"tosindafidi@gmail.com">
2022-03-12 14:13:37 +0100

    smart contract

---
+++ contracts/message.sol
@@ -0,0 +1,14 @@
+//SPECX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.10;
+
+contract Message {
+        String public message;
+
+        function setMessage(string memory _message) public {
+                message - _message;
+        }
+
+        function viewMessage{} public view returns(string memory){
+            return message;
+        }
+    }
